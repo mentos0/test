523e3e6cf65933db47d971dba4c9e0b68d662171
mentos0 <35363922+mentos0@users.noreply.github.com>
2018-03-04 12:55:25 +0800

    tieba pic
    1

---
+++ hh.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+import urllib
+#在python3.3里面，用urllib.request代替urllib2
+import urllib.request
+import re
+
+url = "..."
+page = urllib.request.urlopen(url)
+html = page.read()
+#print(html)    #python3中只能用print(html) python2中能写print html
+
+#正则匹配
+reg = r'src="(.+?\.jpg)" pic_ext'
+imgre = re.compile(reg)
+imglist = re.findall(imgre, html.decode('utf-8'))
+x = 0
+print("start dowload pic")
+for imgurl in imglist:
+    print(imgurl)
+    resp = urllib.request.urlopen(imgurl)
+    respHtml = resp.read()
+    picFile = open('%s.jpg' % x, "wb")
+    picFile.write(respHtml)
+    picFile.close()
+    x = x+1
+print("done")
+
